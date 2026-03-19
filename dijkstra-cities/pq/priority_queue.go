9a64ba46063ec647e781749d96b2ff3eaff9f1f0
Aneeshie <aneeshdas556@gmail.com>
2026-03-19 19:22:27 +0530

    feat: implement heap

---
+++ dijkstra-cities/pq/priority_queue.go
@@ -0,0 +1,33 @@
+package pq
+
+type Item struct {
+	City     string
+	Priority int
+	Index    int
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].Priority < pq[j].Priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].Index = i
+	pq[j].Index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	*pq = append(*pq, x.(*Item))
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[:n-1]
+	return item
+}
